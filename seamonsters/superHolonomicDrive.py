50bebbcaf563471799368b8a8ca570d673cc600d
Jacob van't Hoog <vantjac@gmail.com>
2019-01-22 16:45:09 -0800

    Move circleDistance to seamonsters library

--- seamonsters/superHolonomicDrive.py
+++ seamonsters/superHolonomicDrive.py
@@ -1,19 +1,12 @@
 import math
 import ctre
+import seamonsters as sea
 
 TWO_PI = math.pi * 2
 
 MAX_POSITION_OCCURENCE = 10
 CHECK_ENCODER_CYCLE = 10
-# if circle = math.pi*2, returns the smallest angle between two directions
-# on a circle
-def _circleDistance(a, b, circle):
-    diff = b - a
-    while diff > circle / 2:
-        diff -= circle
-    while diff < -circle / 2:
-        diff += circle
-    return diff
+
 
 def _iteratePairs(list):
     for i in range(0, len(list) - 1):
@@ -367,7 +360,7 @@ class SwerveWheel(Wheel):
     def drive(self, magnitude, direction):
         currentAngle = self._getCurrentSteeringAngle()
         # steering should never rotate more than 90 degrees from any position
-        angleDiff = _circleDistance(currentAngle, direction, math.pi)
+        angleDiff = sea.circleDistance(currentAngle, direction, math.pi)
         self._targetDirection = currentAngle + angleDiff
         #print(math.degrees(currentAngle), math.degrees(self._targetDirection))
         self._setSteering(self._targetDirection)
