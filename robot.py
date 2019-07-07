18ada997cb4024a2b47a4e2bd5b746c9c14a2c70
Trent Bultsma <trentbultsma@gmail.com>
2019-07-07 10:42:37 -0700

    create autonomous template with line using SHD

---
+++ robot.py
@@ -0,0 +1,31 @@
+import seamonsters as sea 
+import wpilib
+import ctre
+import math
+
+class PracticeBot(sea.GeneratorBot):
+
+    def robotInit(self):
+        leftTalon = ctre.WPI_TalonSRX(0)
+        rightTalon = ctre.WPI_TalonSRX(1)
+
+        for talon in [leftTalon, rightTalon]:
+            talon.configSelectedFeedbackSensor(ctre.FeedbackDevice.QuadEncoder, 0, 0)
+        
+        leftWheel = sea.AngledWheel(leftTalon, -1, 0, math.pi/2, 31291.1352, 16)
+        rightWheel = sea.AngledWheel(rightTalon, 1, 0, math.pi/2, 31291.1352, 16)
+
+        self.drivetrain = sea.SuperHolonomicDrive()
+        self.drivetrain.addWheel(leftWheel)
+        self.drivetrain.addWheel(rightWheel)
+
+        for wheel in self.drivetrain.wheels:
+            wheel.driveMode = ctre.ControlMode.PercentOutput
+
+        sea.setSimulatedDrivetrain(self.drivetrain)
+
+    def autonomous(self):
+        pass # put your code here
+
+if __name__ == "__main__":
+    wpilib.run(PracticeBot)
